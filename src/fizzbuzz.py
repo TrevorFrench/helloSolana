3c581c5384fb162b3110339c5e28e4fc0c7e4bee
Trevor French <42419234+TrevorFrench@users.noreply.github.com>
2022-09-29 12:45:52 -0600

    Create fizzbuzz.py

---
+++ src/fizzbuzz.py
@@ -0,0 +1,28 @@
+# fizzbuzz
+# Built with Seahorse v0.1.6
+#
+# On-chain, persistent FizzBuzz!
+
+from seahorse.prelude import *
+
+# This is your program's public key and it will update
+# automatically when you build the project.
+declare_id('BUMTW5vcpg49XBxRMV8Bi6v2JbqzTLPzevLHQBLd3Tx8');
+
+class FizzBuzz(Account):
+  fizz: bool
+  buzz: bool
+  n: u64
+
+@instruction
+def init(owner: Signer, fizzbuzz: Empty[FizzBuzz]):
+  fizzbuzz.init(payer = owner, seeds = ['fizzbuzz', owner])
+
+@instruction
+def do_fizzbuzz(fizzbuzz: FizzBuzz, n: u64):
+  fizzbuzz.fizz = n % 3 == 0
+  fizzbuzz.buzz = n % 5 == 0
+  if not fizzbuzz.fizz and not fizzbuzz.buzz:
+    fizzbuzz.n = n
+  else:
+    fizzbuzz.n = 0
